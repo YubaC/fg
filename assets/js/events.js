fbf5307da9bb71601d98671aab1ee110f268c916
YubaC <2383931696@qq.com>
2022-11-29 15:38:22 +0800

    调整班费扣除的心情值，并添加提示

--- assets/js/events.js
+++ assets/js/events.js
@@ -120,7 +120,7 @@ function classFund() {
         classFundDays = 3;
 
         setTimeout(() => {
-            stringToFormat = [1000 * class_number, classFundNumber + 1, 5 * classFundNumber ** 3];
+            stringToFormat = [1000 * class_number, classFundNumber + 1, 5 * 3 ** classFundNumber];
             nowGameAt = "classFund2";
             nextStep();
             document.getElementById("class").innerHTML = "";
