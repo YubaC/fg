44b56b1346441c506e0a7d8d98aaaddca73d5044
YubaC <49724342+YubaC@users.noreply.github.com>
2023-01-05 16:53:47 +0800

    添加了浏览器提示

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -158,6 +158,40 @@ function IsPc() { //是PC→false，是移动端→true
 assetsToLoad = []; //预加载的资源列表（无须手动编辑）
 
 window.onload = function() {
+    // 提示浏览器
+    var browser = {
+        versions: function() {
+            var u = navigator.userAgent,
+                app = navigator.appVersion;
+            return { //移动终端浏览器版本信息
+                trident: u.indexOf('Trident') > -1, //IE内核
+                presto: u.indexOf('Presto') > -1, //opera内核
+                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+                iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('iPad') > -1, //是否iPad
+                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+            };
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    }
+
+    if (browser.versions.mobile) { //判断是否是移动设备打开。browser代码在下面
+        var ua = navigator.userAgent.toLowerCase(); //获取判断用的对象
+        if (ua.match(/MicroMessenger/i) == "micromessenger") { //在微信中打开
+            window.alert("您正在使用微信内置的浏览器，将无法进行存档，如需存档请在浏览器里打开本网页！");
+        }
+        if (ua.match(/WeiBo/i) == "weibo") { //在新浪微博客户端打开
+            window.alert("您正在使用新浪微博客户端内置的浏览器，将无法进行存档，如需存档请在浏览器里打开本网页！");
+        }
+        if (ua.match(/QQ/i) == "qq") { //在QQ空间打开
+            window.alert("您正在使用QQ内置的浏览器，将无法进行存档，如需存档请在浏览器里打开本网页！");
+        }
+    }
+
     // 隐藏对话框------------
     document.getElementById("dialog").style.display = "none";
     document.getElementById("dialog_musk").style.display = "none";
