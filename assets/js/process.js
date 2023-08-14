63bcb18085cfbee93dca18610f81839dbe56217d
YubaC <2383931696@qq.com>
2023-08-14 20:29:22 +0800

    使用新动画

--- assets/js/process.js
+++ assets/js/process.js
@@ -5,7 +5,7 @@ nextStep()
 */
 
 // 自定义格式化字符串，调用方法：String.format(["","",...,""])
-String.prototype.format = function() {
+String.prototype.format = function () {
     if (arguments.length == 0) {
         return this;
     }
@@ -25,18 +25,19 @@ function say() {
     document.querySelector("#dialog_musk img").src = paraList[speakerAt].img; //说话者的图片
 
     text_to_type = paraList[speakerAt].text.format(stringToFormat);
-    let str_ = ''; //已经打出来的字
+    let str_ = ""; //已经打出来的字
     let i = 0;
-    content = document.getElementById('dialog');
+    content = document.getElementById("dialog");
     paraList.innerHTML = "";
-    timer = setInterval(() => { //一秒打十个字
+    timer = setInterval(() => {
+        //一秒打十个字
         if (str_.length < text_to_type.length) {
             str_ += text_to_type[i++];
-            content.innerHTML = '<p>' + str_ + '_</p>'; //打印时加光标
+            content.innerHTML = "<p>" + str_ + "_</p>"; //打印时加光标
         } else {
             clearInterval(timer);
             textok = true;
-            content.innerHTML = '<p>' + str_ + '</p>';
+            content.innerHTML = "<p>" + str_ + "</p>";
         }
     }, 100);
 }
@@ -59,9 +60,11 @@ function returnChoice(choice) {
 //对做出的选择进行处理
 function continueGame(choice) {
     // console.log(flow.flow[nowGameAt][choice]);
-    if (typeof(flow.flow[nowGameAt]) == "object") { //如果这一段对话有选择
+    if (typeof flow.flow[nowGameAt] == "object") {
+        //如果这一段对话有选择
         nowGameAt = flow.flow[nowGameAt][choice]; //更新对话章节
-    } else { //我 莫得选择
+    } else {
+        //我 莫得选择
         nowGameAt = flow.flow[nowGameAt]; //更新对话章节
     }
     window[nowGameAt](); // 运行这个对话章节所需要执行的函数（函数以该章节命名）
@@ -82,7 +85,8 @@ function showDialog() {
             "#dialog",
             "#dialog_musk",
         ],
-        1, {
+        1,
+        {
             scaleX: 1,
             // scaleY: 1,
             transformOrigin: "left",
@@ -104,19 +108,13 @@ function showChoice() {
     document.getElementById("choice").style.display = "block";
     enable_choice_touch = true;
     tl2 = new TimelineMax();
-    tl2
-        .staggerTo(
-            [
-                "#choice",
-            ],
-            1, {
-                scaleX: 1,
-                transformOrigin: "bottom",
-                ease: Power0,
-                // stagger: 0,
-                duration: 0.1,
-            }
-        )
+    tl2.staggerTo(["#choice"], 1, {
+        // scaleY: 1,
+        height: "auto",
+        transformOrigin: "bottom center",
+        ease: Power0,
+        duration: 0.1,
+    });
 
     // tl2.remove();
 }
@@ -126,22 +124,22 @@ function hideDialog() {
     enable_text_touch = false;
 
     tl2 = new TimelineMax();
-    tl2
-        .staggerTo(
-            [
-                // "#choice",
-                "#dialog",
-                "#dialog_musk",
-            ],
-            1, {
-                scaleX: 0,
-                // scaleY: 0,
-                transformOrigin: "left",
-                ease: Power0,
-                stagger: 0,
-                duration: 0.2,
-            }
-        )
+    tl2.staggerTo(
+        [
+            // "#choice",
+            "#dialog",
+            "#dialog_musk",
+        ],
+        1,
+        {
+            scaleX: 0,
+            // scaleY: 0,
+            transformOrigin: "left",
+            ease: Power0,
+            stagger: 0,
+            duration: 0.2,
+        }
+    );
 
     setTimeout(() => {
         document.getElementById("dialog").style.display = "none";
@@ -154,66 +152,73 @@ function hideDialog() {
 // 隐藏提问的选项
 function hideChoice() {
     tl2 = new TimelineMax();
-    tl2
-        .staggerTo(
-            [
-                "#choice",
-                // "#dialog",
-                // "#dialog_musk",
-            ],
-            1, {
-                scaleX: 0,
-                transformOrigin: "bottom",
-                ease: Power0,
-                stagger: 0,
-                duration: 0.1,
-                onComplete: function() {
-                    document.getElementById("choice").style.display = "none";
-                }
-            }
-        )
+    tl2.staggerTo(
+        [
+            "#choice",
+            // "#dialog",
+            // "#dialog_musk",
+        ],
+        1,
+        {
+            // scaleY: 0,
+            height: 0,
+            transformOrigin: "bottom",
+            ease: Power0,
+            stagger: 0,
+            duration: 0.1,
+        }
+    ).then(() => {
+        document.getElementById("choice").style.display = "none";
+    });
 }
 
 // 换下一句对话用
 function next() {
-    if (enable_text_touch) { //如果允许点击
-        if (!textok) { //如果没打完字 => 补全没打完的字（通过点击的方式加速对话）
+    if (enable_text_touch) {
+        //如果允许点击
+        if (!textok) {
+            //如果没打完字 => 补全没打完的字（通过点击的方式加速对话）
             if (paraList[speakerAt].type == "say") {
                 clearInterval(timer);
-                content.innerHTML = '<p>' + text_to_type + '_</p>';
+                content.innerHTML = "<p>" + text_to_type + "_</p>";
             } else if (paraList[speakerAt].type == "ask") {
                 str_ = text_to_type;
-
             }
             textok = true; //现在打完字了
-
-        } else if (speakerAt < paraList.length - 1 && paraList[speakerAt].type != "ask") { //否则已经打完字了 => 继续下一句话
+        } else if (
+            speakerAt < paraList.length - 1 &&
+            paraList[speakerAt].type != "ask"
+        ) {
+            //否则已经打完字了 => 继续下一句话
             speakerAt += 1;
             if (paraList[speakerAt].type == "say") {
                 hideChoice();
                 say();
-
             } else if (paraList[speakerAt].type == "ask") {
                 ask();
             }
         }
 
-        if (paraList[speakerAt].type == "exit") { //如果下一句话是type="exit"（要求退出对话）
+        if (paraList[speakerAt].type == "exit") {
+            //如果下一句话是type="exit"（要求退出对话）
             hideDialog();
             hideChoice();
-            if (speakerAt < paraList.length - 1) { //如果这条命令后还有命令 => 一定是要执行函数
+            if (speakerAt < paraList.length - 1) {
+                //如果这条命令后还有命令 => 一定是要执行函数
                 speakerAt += 1;
                 // next(); //执行这个函数
             }
         }
 
-        if (paraList[speakerAt].type == "function") { //执行函数
+        if (paraList[speakerAt].type == "function") {
+            //执行函数
             window[paraList[speakerAt].name]();
             // console.log("next");
         }
     }
 
-    if (speakerAt == paraList.length - 1) { //完成对话后清空用于格式化的字符串
+    if (speakerAt == paraList.length - 1) {
+        //完成对话后清空用于格式化的字符串
         stringToFormat = [];
     }
 }
@@ -225,10 +230,10 @@ function ask() {
 
     question = paraList[speakerAt].question;
     text_to_type = str = question.question.format(stringToFormat); //问题文本
-    str_ = ''; //正在打的字
+    str_ = ""; //正在打的字
     let i = 0;
-    content = document.getElementById('dialog');
-    choice = document.getElementById('choice');
+    content = document.getElementById("dialog");
+    choice = document.getElementById("choice");
     paraList.innerHTML = "";
 
     document.querySelector("#dialog_musk img").src = paraList[speakerAt].img;
@@ -237,32 +242,37 @@ function ask() {
     timer = setInterval(() => {
         if (str_.length < str.length) {
             str_ += str[i++];
-            content.innerHTML = '<p>' + str_ + '_</p>'; //打印时加光标
-        } else { //打完字了
+            content.innerHTML = "<p>" + str_ + "_</p>"; //打印时加光标
+        } else {
+            //打完字了
             clearInterval(timer);
-            content.innerHTML = '<p>' + str_ + '</p>';
+            content.innerHTML = "<p>" + str_ + "</p>";
 
             // content.innerHTML += '</div>';
             showChoice(); //显示选项
             textok = true;
             // setTimeout(() => {
             // 构建问题选项------------
-            choice.innerHTML = '';
+            choice.innerHTML = "";
             choice.innerHTML += '<ul id="choices">';
             for (i = 0; i < question.choice.length; i++) {
                 choice.innerHTML += `<li value="${i}" onclick="returnChoice(this.value)">${question.choice[i]}</li>`;
             }
-            choice.innerHTML += '</ul>';
+            choice.innerHTML += "</ul>";
             // ------------
 
             // 如果问题选项宽度过小（不太好看） => 设置宽度100px
             if (document.querySelector("#choice li").clientWidth < 100) {
-                for (i = 0; i < document.querySelectorAll("#choice li").length; i++) {
-                    document.querySelectorAll("#choice li")[i].style.width = "100px";
+                for (
+                    i = 0;
+                    i < document.querySelectorAll("#choice li").length;
+                    i++
+                ) {
+                    document.querySelectorAll("#choice li")[i].style.width =
+                        "100px";
                 }
             }
             // }, 500);
-
         }
     }, 100);
 }
@@ -270,7 +280,8 @@ function ask() {
 // 换大对话章节用
 function nextStep() {
     hideChoice(); //首先隐藏问题选项
-    if (flow.text[nowGameAt] != "none") { //如果有对话
+    if (flow.text[nowGameAt] != "none") {
+        //如果有对话
         showDialog(); //显示对话框
         paraList = flow.text[nowGameAt]; //获取对应的对话列表
         speakerAt = 0;
@@ -283,7 +294,6 @@ function nextStep() {
             ask();
             document.querySelector("html").style.overflow = "hidden";
         }
-
     }
 }
 
@@ -315,14 +325,14 @@ function askSave() {
 
         // document.getElementById("loading_musk").innerHTML += '<div id="upload"><div><input type="file" id="file"><button id="button" onclick="receiveSave()">上传</button></div></div>';
 
-        var fileInput = document.createElement('input');
-        fileInput.type = 'file';
+        var fileInput = document.createElement("input");
+        fileInput.type = "file";
         fileInput.id = "file";
-        fileInput.style.display = 'none';
-        fileInput.onchange = function() {
-                receiveSave(this.files[0]);
-            }
-            // 触发点击事件
+        fileInput.style.display = "none";
+        fileInput.onchange = function () {
+            receiveSave(this.files[0]);
+        };
+        // 触发点击事件
         fileInput.click();
     }, 1000);
 }
@@ -333,12 +343,16 @@ function receiveSave(fileInputed) {
 
     var reader = new FileReader(); //新建一个FileReader
     reader.readAsText(fileInputed, "UTF-8"); //读取文件
-    reader.onload = function(evt) { //读取完文件之后会回来这里
+    reader.onload = function (evt) {
+        //读取完文件之后会回来这里
         var fileString = evt.target.result; // 读取文件内容
         console.log(fileString);
 
         // 检查存档是否完好
-        if (fileString.search("version") != -1 && JSON.parse(fileString).version == flow.version) {
+        if (
+            fileString.search("version") != -1 &&
+            JSON.parse(fileString).version == flow.version
+        ) {
             console.log("in3-");
             justLoadedFromSave = true;
             alert("读取成功！");
@@ -361,7 +375,13 @@ function receiveSave(fileInputed) {
             grade3 = loadedSave.grade3;
             grade3Special = loadedSave.grade3Special;
 
-            class_number = grade1 + grade2 + grade3 + grade1Special + grade2Special + grade3Special; //当前班级数
+            class_number =
+                grade1 +
+                grade2 +
+                grade3 +
+                grade1Special +
+                grade2Special +
+                grade3Special; //当前班级数
 
             diningHallLevel - loadedSave.diningHallLevel;
             dormitoryLevel = loadedSave.dormitoryLevel;
@@ -383,7 +403,9 @@ function receiveSave(fileInputed) {
             path_list = loadedSave.path_list;
             point_list = [];
             for (i = 0; i < loadedSave.point_list.length; i++) {
-                point_list.push(document.querySelector(loadedSave.point_list[i]));
+                point_list.push(
+                    document.querySelector(loadedSave.point_list[i])
+                );
             }
 
             for (i = 0; i < point_list.length - 1; i++) {
@@ -393,8 +415,9 @@ function receiveSave(fileInputed) {
                 to_y = point_list[i + 1].cy["animVal"]["valueAsString"];
                 // console.log(from_x, from_y, to_x, to_y);
 
-                document.getElementById("path_lines").innerHTML +=
-                    `<line x1="${from_x}" y1="${from_y}" x2="${to_x}" y2="${to_y}" style="stroke:#fa9668; stroke-width:3px; "></line>`;
+                document.getElementById(
+                    "path_lines"
+                ).innerHTML += `<line x1="${from_x}" y1="${from_y}" x2="${to_x}" y2="${to_y}" style="stroke:#fa9668; stroke-width:3px; "></line>`;
             }
 
             clicked1 = point_list[point_list.length - 1];
@@ -406,7 +429,6 @@ function receiveSave(fileInputed) {
                 fadeOut(document.querySelector("#loading_musk"), 40, 0);
                 // fadeOut(document.querySelector("#loading_musk p"), 40, 0);
                 // startGame();
-
             }, 1000);
 
             setTimeout(() => {
@@ -433,7 +455,7 @@ function receiveSave(fileInputed) {
             // showDialog();
             // showChoice();
         }
-    }
+    };
 }
 
 // 回答“找不到工作日志”后的回调函数
@@ -468,7 +490,6 @@ function reStart() {
         fadeOut(document.querySelector("#loading_musk"), 40, 0);
         // fadeOut(document.querySelector("#loading_musk p"), 40, 0);
         // startGame();
-
     }, 1000);
 
     setTimeout(() => {
@@ -531,7 +552,6 @@ function loadFromCookie() {
             fadeOut(document.querySelector("#loading_musk"), 40, 0);
             // fadeOut(document.querySelector("#loading_musk p"), 40, 0);
             // startGame();
-
         }, 1000);
 
         setTimeout(() => {
@@ -553,7 +573,9 @@ function showDay() {
 
     document.querySelector("#day h1").innerHTML = "DAY " + day;
 
-    document.querySelector("#day p").innerHTML = `今日空气污染：${airPollution}&emsp;学期日${todayInTerm}/${term}`;
+    document.querySelector(
+        "#day p"
+    ).innerHTML = `今日空气污染：${airPollution}&emsp;学期日${todayInTerm}/${term}`;
 
     // 对话框会在1s后关掉musk，这里补一下
     setTimeout(() => {
@@ -561,13 +583,13 @@ function showDay() {
     }, 1000);
 
     timeline = new TimelineMax({
-        onComplete: function() {
-            console.log("finised")
+        onComplete: function () {
+            console.log("finised");
             document.getElementById("musk").style.display = "none";
         },
-        onStart: function() {
+        onStart: function () {
             document.getElementById("musk").style.display = "block";
-        }
+        },
     });
 
     timeline.to("#day", 1, {
@@ -584,7 +606,6 @@ function showDay() {
         stagger: 0,
         delay: 1,
     });
-
 }
 
 // 新手教程
@@ -599,20 +620,22 @@ function teachPlay() {
 function getCSSPath(node) {
     let parts = [];
     while (node.parentElement) {
-        let str = node.tagName.toLowerCase()
+        let str = node.tagName.toLowerCase();
         if (node.id) {
             str += `#${node.id}`;
             parts.unshift(str);
             break;
         }
 
-        let siblingsArr = Array.prototype.slice.call(node.parentElement.childNodes);
+        let siblingsArr = Array.prototype.slice.call(
+            node.parentElement.childNodes
+        );
         let ind = siblingsArr.filter((n) => n.attributes).indexOf(node);
         parts.unshift(str + `:nth-child(${ind + 1})`);
         node = node.parentElement;
     }
 
-    return parts.join(' > ');
+    return parts.join(" > ");
 }
 
 // 保存
@@ -635,52 +658,50 @@ function save(saveType) {
     }
 
     let data = {
-        "version": flow.version,
-        "todayInTerm": todayInTerm, //今天是这个学期中的第几天
+        version: flow.version,
+        todayInTerm: todayInTerm, //今天是这个学期中的第几天
 
-        "grade1": grade1,
-        "grade1Special": grade1Special,
-        "grade2": grade2,
-        "grade2Special": grade2Special,
-        "grade3": grade3,
-        "grade3Special": grade3Special,
+        grade1: grade1,
+        grade1Special: grade1Special,
+        grade2: grade2,
+        grade2Special: grade2Special,
+        grade3: grade3,
+        grade3Special: grade3Special,
 
-        "stain": stain, //满100失业
+        stain: stain, //满100失业
 
-        "complainDays": complainDays, // >0 => 投诉处理中，处理期间暂不受理新的投诉，处理期间收入减半
+        complainDays: complainDays, // >0 => 投诉处理中，处理期间暂不受理新的投诉，处理期间收入减半
 
-        "expect1": expect1, //上级教育机构预期的封口费
-        "expect2": expect2, //媒体预期的封口费
+        expect1: expect1, //上级教育机构预期的封口费
+        expect2: expect2, //媒体预期的封口费
 
-        "complainedBefore": complainedBefore,
+        complainedBefore: complainedBefore,
 
-        "diningHallLevel": diningHallLevel,
-        "dormitoryLevel": dormitoryLevel,
+        diningHallLevel: diningHallLevel,
+        dormitoryLevel: dormitoryLevel,
 
         // "class_number": class_number, //当前班级数
-        "day": day,
-        "mood": mood,
-        "money": money,
-        "speed": speed_now,
-        "path_list": path_list,
-        "point_list": pointToSave
+        day: day,
+        mood: mood,
+        money: money,
+        speed: speed_now,
+        path_list: path_list,
+        point_list: pointToSave,
     };
     var content = JSON.stringify(data); //JSON格式化保存
 
     switch (saveType) {
-        case ("json"):
+        case "json":
             var blob = new Blob([content], {
-                type: "text/plain;charset=utf-8"
+                type: "text/plain;charset=utf-8",
             });
             saveAs(blob, `${time}某高中校长工作日志.json`);
             setCookie("mapSaved", content, 30 * 365);
             break;
-        case ("cookie"):
+        case "cookie":
             setCookie("mapSaved", content, 30 * 365);
             break;
     }
-
-
 }
 
 // 初始化游戏
@@ -695,7 +716,6 @@ function startGame() {
             fadeOut(document.querySelector("#loading_musk"), 40, 0);
             // fadeOut(document.querySelector("#loading_musk p"), 40, 0);
             // startGame();
-
         }, 1000);
 
         setTimeout(() => {
@@ -705,7 +725,6 @@ function startGame() {
             document.getElementById("go").disabled = false;
             tl.play();
         }, 2000);
-
     } else {
         paraList = flow.text[nowGameAt];
         // return flow;
